d43fe021bda82782bf791afb7051b85d5de045b0
Josh Vera <vera@github.com>
2014-05-18 11:07:24 -0400

    dots. jss loves dots

--- Classes/VVVAppDelegate.m
+++ Classes/VVVAppDelegate.m
@@ -12,9 +12,9 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-	NSURL *idrisAppURL = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"js"];
+	NSURL *idrisAppURL = [NSBundle.mainBundle URLForResource:@"main" withExtension:@"js"];
 	NSString *idrisApp = [[NSString alloc] initWithContentsOfURL:idrisAppURL encoding:NSUTF8StringEncoding error:NULL];
-	[[Mocha sharedRuntime] evalString:idrisApp];
+	[Mocha.sharedRuntime evalString:idrisApp];
     return YES;
 }
 							
